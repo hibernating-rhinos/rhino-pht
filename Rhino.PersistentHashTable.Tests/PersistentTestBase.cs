faf083a72dd58c7352211c7ad604570ad83977d9
Corey Kaylor <corey@kaylors.net>
2011-07-26 21:38:10 -0600

    Made remove directory more reliable in tests for build server.

--- Rhino.PersistentHashTable.Tests/PersistentTestBase.cs
+++ Rhino.PersistentHashTable.Tests/PersistentTestBase.cs
@@ -8,8 +8,28 @@ namespace Rhino.PersistentHashTable.Tests
 
 		public PersistentTestBase()
 		{
-			if (Directory.Exists(testDatabase))
-				Directory.Delete(testDatabase, true);
+            if (Directory.Exists(testDatabase))
+                DeleteDirectory(testDatabase);
 		}
+
+
+        private static void DeleteDirectory(string rootDir)
+        {
+            var files = Directory.GetFiles(rootDir);
+            var subDirectories = Directory.GetDirectories(rootDir);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in subDirectories)
+            {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(rootDir, false);
+        }
 	}
 }
